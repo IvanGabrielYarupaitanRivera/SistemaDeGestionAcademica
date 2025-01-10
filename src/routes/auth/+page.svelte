d8558bd537fbfc6efaa1d9b3d7a8bbc8cc911479
Vanchi <ivangyr321@gmail.com>
2025-01-09 20:57:43 -0500

    Agregar formulario de inicio de sesión en +page.svelte

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Inicio de Sesión</h1>
+
+<form method="POST" action="?/login">
+	<label>
+		Email
+		<input name="email" type="email" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" />
+	</label>
+	<button>Login</button>
+	<button formaction="?/signup">Sign up</button>
+</form>
