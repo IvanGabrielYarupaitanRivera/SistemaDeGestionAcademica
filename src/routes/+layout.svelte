a7266f18ae55a18ae6ffdbe95171d63d70b82748
Vanchi <ivangyr321@gmail.com>
2025-01-09 20:57:16 -0500

    Agregar manejo de cambios de sesión de Supabase y navegación en +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
 	import '../app.css';
-	let { children } = $props();
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/auth">Ingresar</a>
+		<a href="/private">Privado</a>
+	</nav>
+</header>
 
 {@render children()}
