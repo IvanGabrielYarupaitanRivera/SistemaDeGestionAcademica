1c57a7f1bac54a6fd09316260d016692cbe64e0a
Vanchi <ivangyr321@gmail.com>
2025-01-10 14:38:51 -0500

    Eliminar variables de sesión y usuario en la carga de notas en la ruta privada

--- src/routes/private/+page.server.ts
+++ src/routes/private/+page.server.ts
@@ -1,11 +1,8 @@
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ depends, locals: { supabase, session, user } }) => {
+export const load: PageServerLoad = async ({ depends, locals: { supabase } }) => {
 	depends('supabase:db:notes');
 	const { data: notes } = await supabase.from('notes').select('id,note').order('id');
-
-	console.log('Datos');
-	console.log(supabase, session?.access_token, user?.email);
 
 	return { notes: notes ?? [] };
 };
