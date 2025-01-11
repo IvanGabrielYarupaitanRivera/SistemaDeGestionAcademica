8ac7db224a6db4699284aaa9d527ac15596b6678
Vanchi <ivangyr321@gmail.com>
2025-01-11 18:19:19 -0500

    Agregar funciones para obtener y crear perfiles en la base de datos

---
+++ src/lib/database/perfiles/db.ts
@@ -0,0 +1,22 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Perfil } from './type';
+
+export const PerfilDB = {
+	async obtenerPerfil(supabase: SupabaseClient, id: string): Promise<{ perfil: Perfil }> {
+		const { data, error } = await supabase.from('Perfiles').select('*').eq('id', id).single();
+
+		if (error) {
+			throw new Error('Error al obtener el perfil.');
+		}
+
+		return { perfil: data };
+	},
+
+	async crearPerfil(supabase: SupabaseClient, perfil: Perfil) {
+		const { error } = await supabase.from('Perfiles').insert([perfil]);
+
+		if (error) {
+			throw new Error('Error al crear el perfil');
+		}
+	}
+};
