651611eac90c9258bb520420fb5cfb672a62ed7c
Vanchi <ivangyr321@gmail.com>
2025-01-11 03:43:16 -0500

    Agregar tipos y interfaz para la gestión de perfiles de usuario

---
+++ src/lib/database/perfiles/type.ts
@@ -0,0 +1,13 @@
+export type RolUsuario = 'Administrador' | 'Estudiante' | 'Profesor';
+
+export interface Perfil {
+	id: string;
+	nombres: string;
+	apellido_paterno: string;
+	apellido_materno: string;
+	dni: string;
+	rol: RolUsuario;
+	email: string;
+	fecha_creacion: string;
+	fecha_actualizacion: string;
+}
