7ca14f9c0ef09612bc3e058f2146de40d98b401f
Vanchi <ivangyr321@gmail.com>
2025-01-10 19:25:02 -0500

    Eliminar carga de datos de países y actualizar título en la página de inicio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<script>
-	let { data } = $props();
-	let { countries } = $derived(data);
-</script>
-
-<h1>Welcome to Supabase!</h1>
-<ul>
-	{#each countries as country}
-		<li>{country.name}</li>
-	{/each}
-</ul>
+<h1>PÁGINA DE INICIO</h1>
