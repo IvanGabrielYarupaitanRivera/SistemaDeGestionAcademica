179b245e7989b504ab179a2307e7471b334f6d13
Vanchi <ivangyr321@gmail.com>
2025-01-11 03:43:07 -0500

    Agregar funcionalidad para registrar e iniciar sesión de usuarios en la base de datos

---
+++ src/lib/database/usuarios/db.ts
@@ -0,0 +1,23 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Usuario } from './type';
+import { validar } from './validaciones';
+
+export const UsuarioDB = {
+	async registrarUsuario(supabase: SupabaseClient, { email, password }: Usuario) {
+		validar.validarEmail(email);
+		validar.validarPassword(password);
+
+		const { error } = await supabase.auth.signUp({ email, password });
+
+		if (error) throw new Error(`Error al registrar el usuario.`);
+	},
+
+	async iniciarSesion(supabase: SupabaseClient, { email, password }: Usuario) {
+		validar.validarEmail(email);
+		validar.validarPassword(password);
+
+		const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+		if (error) throw new Error('Error al iniciar sesión.');
+	}
+};
