f48135b89c7e5b087a7b816ebb2d432e62b12390
Vanchi <ivangyr321@gmail.com>
2025-01-11 18:19:30 -0500

    Agregar estructura y encabezados al perfil de usuario, incluyendo secciones para datos y un botón de edición

--- src/routes/privado/perfil/+page.svelte
+++ src/routes/privado/perfil/+page.svelte
@@ -1,1 +1,44 @@
-<h1>Bienvenido a tu perfil</h1>
+<script lang="ts">
+	import type { Perfil } from '$lib/database/perfiles/type';
+</script>
+
+<header>
+	<h1 class="text-2xl font-bold">Mi Perfil</h1>
+	<hr />
+</header>
+
+<section aria-labelledby="datos-perfil">
+	<h2 id="datos-perfil" class="sr-only">Datos del perfil</h2>
+	<dl>
+		<dt>ID</dt>
+		<!-- <dd>{perfil.id}</dd> -->
+
+		<dt>Nombres</dt>
+		<!-- <dd>{perfil.nombres}</dd> -->
+
+		<dt>Apellido Paterno</dt>
+		<!-- <dd>{perfil.apellido_paterno}</dd> -->
+
+		<dt>Apellido Materno</dt>
+		<!-- <dd>{perfil.apellido_materno}</dd> -->
+
+		<dt>DNI</dt>
+		<!-- <dd>{perfil.dni}</dd> -->
+
+		<dt>Rol de Usuario</dt>
+		<!-- <dd>{perfil.rol}</dd> -->
+
+		<dt>Email</dt>
+		<!-- <dd>{perfil.email}</dd> -->
+
+		<dt>Fecha de Creación</dt>
+		<!-- <dd>{perfil.fecha_creacion}</dd> -->
+
+		<dt>Fecha de Actualización</dt>
+		<!-- <dd>{perfil.fecha_actualizacion}</dd> -->
+	</dl>
+</section>
+
+<footer>
+	<button type="button">Editar Información</button>
+</footer>
