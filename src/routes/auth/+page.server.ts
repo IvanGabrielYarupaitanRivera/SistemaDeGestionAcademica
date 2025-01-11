69d4623317a358a6debcc67793e053a2d1c1cafe
Vanchi <ivangyr321@gmail.com>
2025-01-11 03:43:31 -0500

    Refactor la gestión de registro e inicio de sesión de usuarios utilizando la base de datos

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,42 +1,40 @@
-import { fail, redirect } from '@sveltejs/kit';
-
+import { fail } from '@sveltejs/kit';
+import { UsuarioDB } from '$lib/database/usuarios/db';
+import type { Usuario } from '$lib/database/usuarios/type';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
 	signup: async ({ request, locals: { supabase } }) => {
 		const formData = await request.formData();
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
+
+		const usuario: Usuario = {
+			email: formData.get('email') as string,
+			password: formData.get('password') as string
+		};
 
 		try {
-			const { error: signUpError } = await supabase.auth.signUp({ email, password });
-
-			if (signUpError) {
-				return fail(500, {
-					error: signUpError.message
-				});
-			} else {
-				redirect(303, '/');
-			}
-		} catch (error) {
+			await UsuarioDB.registrarUsuario(supabase, usuario);
+		} catch (err) {
 			return fail(500, {
-				error: 'Error en el servidor' + error
+				error: err instanceof Error ? err.message : 'Error en el servidor'
 			});
 		}
 	},
 
 	login: async ({ request, locals: { supabase } }) => {
 		const formData = await request.formData();
-		const email = formData.get('email') as string;
-		const password = formData.get('password') as string;
 
-		const { error } = await supabase.auth.signInWithPassword({ email, password });
+		const usuario: Usuario = {
+			email: formData.get('email') as string,
+			password: formData.get('password') as string
+		};
 
-		if (error) {
-			console.error(error);
-			redirect(303, '/auth/error');
-		} else {
-			redirect(303, '/private');
+		try {
+			await UsuarioDB.iniciarSesion(supabase, usuario);
+		} catch (err) {
+			return fail(500, {
+				error: err instanceof Error ? err.message : 'Error en el servidor'
+			});
 		}
 	}
 };
