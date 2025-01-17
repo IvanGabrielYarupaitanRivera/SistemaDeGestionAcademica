a0712d0bc6420c6d8e3c76dfbfa3829f35798ae8
Ivan <ivangyr321@gmail.com>
2025-01-17 11:39:49 -0500

    Eliminar importación no utilizada de la transición 'fade' en +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,9 @@
 	import Hero from './components/Hero/Hero.svelte';
 	import Main from './components/Main/Main.svelte';
 	import Footer from './components/Footer/Footer.svelte';
-	import { fade } from 'svelte/transition';
 </script>
 
 <Header />
-
 <Hero />
-
 <Main />
-
 <Footer />
