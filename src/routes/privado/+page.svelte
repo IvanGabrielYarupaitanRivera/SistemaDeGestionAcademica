9bbf83abfb8c392c0d1bd67da8324b6375742ed7
Ivan <ivangyr321@gmail.com>
2025-01-17 11:49:22 -0500

    Eliminar variable innecesaria en la página privada

--- src/routes/privado/+page.svelte
+++ src/routes/privado/+page.svelte
@@ -2,8 +2,6 @@
 	import Header from './components/HeaderPrivado/Header.svelte';
 	import Hero from './components/HeroPrivado/Hero.svelte';
 	import Main from './components/MainPrivado/Main.svelte';
-
-	let { data } = $props();
 </script>
 
 <Header />
