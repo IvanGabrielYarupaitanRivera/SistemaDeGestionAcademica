b22033adaef2972003b0a22341e64024c4d60001
Ivan <ivangyr321@gmail.com>
2025-01-24 21:56:04 -0500

    Agregar acción para crear usuarios con validación de datos en el servidor

---
+++ src/routes/privado/administrador/perfiles/+page.server.ts
@@ -0,0 +1,38 @@
+import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { PRIVATE_SUPABASE_SERVICE_ROLE } from '$env/static/private';
+import type { Usuario } from '$lib/database/usuarios/type';
+import type { RolUsuario } from '$lib/database/perfiles/type';
+
+import { UsuarioDB } from '$lib/database/usuarios/db';
+
+// Cliente Supabase
+const supabaseAdmin = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_ROLE);
+
+export const actions = {
+	crearUsuario: async ({ request }) => {
+		const formData = await request.formData();
+
+		// Validar datos del formulario
+		const usuario: Usuario = {
+			email: formData.get('email')?.toString() ?? '',
+			password: formData.get('password')?.toString() ?? ''
+		};
+
+		const rol = formData.get('rol')?.toString() as RolUsuario;
+
+		try {
+			await UsuarioDB.crearUsuario(supabaseAdmin, usuario, rol);
+
+			return {
+				success: 'Usuario creado exitosamente.'
+			};
+		} catch (error) {
+			return fail(500, {
+				error: error instanceof Error ? error.message : 'Error al crear el usuario'
+			});
+		}
+	}
+} satisfies Actions;
