58d444d20d07cc2bcd088b7b7b1deb7473ecf8f8
Ivan <ivangyr321@gmail.com>
2025-01-24 22:40:06 -0500

    Importar la variable 'PRIVATE_SUPABASE_SERVICE_ROLE' en el archivo +page.server.ts

--- src/routes/privado/administrador/perfiles/+page.server.ts
+++ src/routes/privado/administrador/perfiles/+page.server.ts
@@ -2,12 +2,13 @@ import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { PRIVATE_SUPABASE_SERVICE_ROLE } from '$env/static/private';
 import type { Usuario } from '$lib/database/usuarios/type';
 import type { RolUsuario } from '$lib/database/perfiles/type';
 import { UsuarioDB } from '$lib/database/usuarios/db';
 
 const supabaseAdmin = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_ROLE);
+
+import { PRIVATE_SUPABASE_SERVICE_ROLE } from '$env/static/private';
 
 export const actions = {
 	crearUsuario: async ({ request }) => {
