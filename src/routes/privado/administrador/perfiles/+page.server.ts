ff07bb72b700651798f08f4be72655e98403c7d9
Ivan <ivangyr321@gmail.com>
2025-01-25 11:41:17 -0500

    Agregar función para editar perfiles y manejar errores en la actualización

--- src/routes/privado/administrador/perfiles/+page.server.ts
+++ src/routes/privado/administrador/perfiles/+page.server.ts
@@ -4,11 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 import { UsuarioDB } from '$lib/database/usuarios/db';
 
 import type { Usuario } from '$lib/database/usuarios/type';
-import type { RolUsuario } from '$lib/database/perfiles/type';
+import type { Perfil, RolUsuario } from '$lib/database/perfiles/type';
 
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { PRIVATE_SUPABASE_SERVICE_ROLE } from '$env/static/private';
 import { PerfilDB } from '$lib/database/perfiles/db';
+import { perfilStore } from '$lib/stores/perfil';
 
 const supabaseAdmin = createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_SERVICE_ROLE);
 
@@ -47,6 +48,36 @@ export const actions = {
 		} catch (error) {
 			return fail(500, {
 				error: error instanceof Error ? error.message : 'Error al crear el usuario'
+			});
+		}
+	},
+
+	editarUsuario: async ({ request, locals: { supabase, user } }) => {
+		const formData = await request.formData();
+
+		if (!user) {
+			return fail(401, { error: 'No autorizado' });
+		}
+
+		const perfil: Perfil = {
+			id: formData.get('id') as string,
+			nombres: formData.get('nombres') as string,
+			apellido_paterno: formData.get('apellido_paterno') as string,
+			apellido_materno: formData.get('apellido_materno') as string,
+			dni: formData.get('dni') as string,
+			rol: formData.get('rol') as RolUsuario,
+			fecha_actualizacion: new Date().toISOString()
+		};
+
+		try {
+			await PerfilDB.editarPerfil(supabase, perfil.id, perfil);
+
+			perfilStore.set(null);
+
+			return { success: 'Perfil actualizado correctamente' };
+		} catch (err) {
+			return fail(500, {
+				error: err instanceof Error ? err.message : 'Error en el servidor'
 			});
 		}
 	}
