dd5903f0bd98a10d5763929151f2c97eef545881
Ivan <ivangyr321@gmail.com>
2025-01-27 15:21:12 -0500

    Agregar funcionalidad para inscribir estudiantes en cursos y obtener lista de cursos en la carga de estudiantes

--- src/routes/privado/estudiantes/+page.server.ts
+++ src/routes/privado/estudiantes/+page.server.ts
@@ -2,15 +2,18 @@ import { EstudianteDB } from '$lib/database/estudiantes/db';
 import { error, fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
 import type { Estudiante } from '$lib/database/estudiantes/type';
+import { InscripcionDB } from '$lib/database/inscripciones/db';
+import { CursoDB } from '$lib/database/cursos/db';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const { supabase } = locals;
 
 	try {
 		const estudiantes = await EstudianteDB.obtenerEstudiantes(supabase);
+		const cursos = await CursoDB.obtenerCursos(supabase);
 		console.log('Obteniendo estudiantes desde la base de datos');
 
-		return { estudiantes };
+		return { estudiantes, cursos };
 	} catch (err) {
 		console.error('❌ Error inesperado:', err);
 		throw error(500, 'Error al obtener estudiantes');
@@ -35,6 +38,21 @@ export const actions = {
 		} catch (error) {
 			return fail(500, {
 				error: error instanceof Error ? error.message : 'Error al editar el estudiante'
+			});
+		}
+	},
+
+	inscribirEnCursos: async ({ request, locals: { supabase } }) => {
+		const formData = await request.formData();
+		const estudiante_id = formData.get('estudiante_id') as string;
+		const cursos_ids = formData.getAll('cursos_ids') as string[];
+
+		try {
+			await InscripcionDB.inscribirEnCursos(supabase, estudiante_id, cursos_ids);
+			return { success: 'Inscripciones actualizadas exitosamente' };
+		} catch (error) {
+			return fail(500, {
+				error: error instanceof Error ? error.message : 'Error al actualizar inscripciones'
 			});
 		}
 	}
